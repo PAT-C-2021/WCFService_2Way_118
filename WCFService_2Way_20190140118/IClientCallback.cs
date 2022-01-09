43f545a9ad348c543a5856e379d0771c532840ec
Muhammad Adrianto <isfan.diar123@gmail.com>
2022-01-09 20:57:19 +0700

    Add project files.

---
+++ WCFService_2Way_20190140118/IClientCallback.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WCFService_2Way_20190140118
+{
+    [ServiceContract]
+    interface IClientCallback
+    {
+        [OperationContract(IsOneWay = true)]
+        void pesamKirim(string user, string pesan);
+    }
+}
---
+++ WCFService_2Way_20190140118/IServiceCallback.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace WCFService_2Way_20190140118
+{
+   
+    [ServiceContract(CallbackContract = typeof(IClientCallback))]
+    public interface IServiceCallback
+    {
+        [OperationContract(IsOneWay = true)]
+        void gabung(string username);
+
+        [OperationContract(IsOneWay = true)]
+        void kirimPesan(string pesan);
+
+    }
+}
